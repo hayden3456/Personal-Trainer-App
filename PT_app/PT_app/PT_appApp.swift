2d222734faa565711daca029f94e9c415d388657
h <hjconsts@gmail.com>
2021-03-27 12:49:50 -0500

    first commit

---
+++ PT_app/PT_app/PT_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  PT_appApp.swift
+//  PT_app
+//
+//  Created by Hayden Constas on 3/27/21.
+//
+
+import SwiftUI
+
+@main
+struct PT_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
